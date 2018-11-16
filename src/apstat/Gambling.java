808ae7e4594787a4e1195078c0707a44b1c306c5
ApolloZhu <public-apollonian@outlook.com>
2018-11-16 08:15:17 -0500

    Simplify
    - Use constant SIZE
    - Fix wrong implementation of sleep (and remove param)
    - Remove inner classes in favor of closures
    - Create color and color convertion constants

--- src/apstat/Gambling.java
+++ src/apstat/Gambling.java
@@ -1,141 +1,122 @@
 package apstat;
+
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 public class Gambling extends JPanel {
-	private JButton[][] board;
-	private int[][] matrix;
+	private static final int SIZE = 10;
+	private JButton[][] board = new JButton[SIZE][SIZE];
 	private JLabel label;
-	private int turns, winnings;
 	private JButton play;
 
+	private int[][] matrix = new int[SIZE][SIZE];
+	// initialize in play:
+	private int turns, winnings;
+
 	public Gambling() {
-		turns = 3;
-		winnings = 0;
 		setLayout(new BorderLayout());
 		JPanel north = new JPanel();
-		north.setLayout(new FlowLayout());
 		add(north, BorderLayout.NORTH);
 		label = new JLabel("Pay 4 then click \"play\" to generate treasure.");
 		north.add(label);
 
 		JPanel center = new JPanel();
-		center.setLayout(new GridLayout(10, 10));
+		center.setLayout(new GridLayout(SIZE, SIZE));
 		add(center, BorderLayout.CENTER);
-		board = new JButton[10][10];
-		matrix = new int[10][10];
-		for (int r = 0; r < 10; r++)
-			for (int c = 0; c < 10; c++) {
+		for (int r = 0; r < SIZE; r++) {
+			for (int c = 0; c < SIZE; c++) {
 				matrix[r][c] = 0;
 				board[r][c] = new JButton();
-				board[r][c].setBackground(new Color(153, 153, 153));
-				board[r][c].addActionListener(new Handler1(r, c));
+				board[r][c].setBackground(Color.GRAY);
+				int row = r, col = c; // capture
+				board[r][c].addActionListener(e -> clicked(row, col));
+				board[r][c].setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
 				board[r][c].setBorderPainted(false);
 				board[r][c].setEnabled(false);
 				center.add(board[r][c]);
 			}
+		}
+
 		play = new JButton("Play");
-		play.addActionListener(new Handler2());
+		play.addActionListener(this::play);
 		add(play, BorderLayout.SOUTH);
 	}
 
-	private class Handler1 implements ActionListener {
-		private int r, c;
+	private void clicked(int r, int c) {
+		board[r][c].setEnabled(false);
+		winnings += matrix[r][c];
+		board[r][c].setBackground(colorFor[matrix[r][c]]);
+		board[r][c].paintImmediately(0, 0, 1000, 1000);
+		label.setText("You have " + --turns + " turn" + (turns == 1 ? "" : "s") + " to uncover treasure.");
+		board[r][c].setBorderPainted(true);
+		if (turns == 0)
+			endGame();
+	}
 
-		public Handler1(int r, int c) {
-			this.r = r;
-			this.c = c;
-		}
-
-		public void actionPerformed(ActionEvent e) {
-			board[r][c].setEnabled(false);
-			winnings += matrix[r][c];
-			if (matrix[r][c] == 0)
-				board[r][c].setBackground(new Color(150, 104, 66));// brown
-			else if (matrix[r][c] == 2)
-				board[r][c].setBackground(new Color(92, 255, 86));// green
-			else if (matrix[r][c] == 3)
-				board[r][c].setBackground(new Color(255, 8, 0));// red
-			else
-				board[r][c].setBackground(new Color(0, 127, 255));// blue
-			board[r][c].paintImmediately(0, 0, 1000, 1000);
-			turns--;
-			label.setText("You have " + (turns == 1 ? 1 + " turn " : turns + " turns ") + "to uncover treasure.");
-			board[r][c].setBorderPainted(true);
-			if (turns == 0) {
-				label.setText("You won " + winnings + " dollars! To generate more treasure, pay 4 and click \"play\"");
-				winnings = 0;
-				turns = 3;
-				play.setEnabled(true);
-				for (int r = 0; r < board.length; r++)
-					for (int c = 0; c < board.length; c++) {
-						board[r][c].setEnabled(false);
-						/*
-						 * if(board[r][c].getBackground().equals(new Color(255,251,150)))
-						 * board[r][c].setBackground(new Color(153,153,153));
-						 */
-
-						if (matrix[r][c] == 0)
-							board[r][c].setBackground(new Color(150, 104, 66));// brown
-						else if (matrix[r][c] == 2)
-							board[r][c].setBackground(new Color(92, 255, 86));// green
-						else if (matrix[r][c] == 3)
-							board[r][c].setBackground(new Color(255, 8, 0));// red
-						else if (matrix[r][c] == 4)
-							board[r][c].setBackground(new Color(0, 127, 255));// blue
-						else if (matrix[r][c] == 5)
-							board[r][c].setBackground(new Color(150, 104, 66));// h brown
-						else if (matrix[r][c] == 7)
-							board[r][c].setBackground(new Color(150, 104, 66)); // h blue
-						else if (matrix[r][c] == 8)
-							board[r][c].setBackground(new Color(150, 104, 66)); // h blue
-						else
-							board[r][c].setBackground(new Color(150, 104, 66)); // h blue
-
-						board[r][c].paintImmediately(0, 0, 1000, 1000);
-						sleep(100);
-					}
+	private void endGame() {
+		label.setText("You won " + winnings + " dollars! To generate more treasure, pay 4 and click \"play\"");
+		play.setEnabled(true);
+		for (int r = 0; r < SIZE; r++) {
+			for (int c = 0; c < SIZE; c++) {
+				board[r][c].setEnabled(false);
+				board[r][c].setBackground(colorFor[matrix[r][c]]);
+				board[r][c].paintImmediately(0, 0, 1000, 1000);
+				sleep();
 			}
 		}
 	}
 
-	private class Handler2 implements ActionListener {
-		public void actionPerformed(ActionEvent e) {
-			play.setEnabled(false);
-			label.setText("You have " + turns + " turns to uncover treasure.");
-			for (int c = 0; c < board.length; c++)
-				for (int r = 0; r < board.length; r++) {
+	private void play(ActionEvent e) {
+		play.setEnabled(false);
+		winnings = 0;
+		label.setText("You have " + (turns = 3) + " turns to uncover treasure.");
+		for (int c = 0; c < SIZE; c++) {
+			for (int r = 0; r < SIZE; r++) {
+				matrix[r][c] = 0;
+				board[r][c].setBorderPainted(false);
+				board[r][c].setEnabled(true);
+				board[r][c].setBackground(UNREVEALED);
+				board[r][c].paintImmediately(0, 0, 1000, 1000);
+				sleep();
+				double n = Math.random() * 28;
+				if (n < 1)
+					matrix[r][c] = 4;
+				else if (n < 7)
+					matrix[r][c] = 3;
+				else if (n < 14)
+					matrix[r][c] = 2;
+				else
 					matrix[r][c] = 0;
-					board[r][c].setBorderPainted(false);
-					board[r][c].setEnabled(true);
-					board[r][c].setBackground(new Color(255, 251, 150));
-					board[r][c].paintImmediately(0, 0, 1000, 1000);
-					sleep(100);
-					double n = Math.random();
-					if (n < 1 / 28.0)
-						matrix[r][c] = 4;// blue
-					else if (n < 7 / 28.0)
-						matrix[r][c] = 3;// red
-					else if (n < 14 / 28.0)
-						matrix[r][c] = 2;
-					else
-						matrix[r][c] = 0;
-				}
+			}
 		}
-
 	}
 
-	private void sleep(int millis) {
-		do {
-		} while (System.nanoTime() / 100000 % millis != 0);
+	private void sleep() {
+		try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
+	// Colors
+
+	private static final Color UNREVEALED = new Color(255, 251, 150);
+
+	private static final Color BROWN = new Color(150, 104, 66);
+	private static final Color GREEN = new Color(92, 255, 86);
+	private static final Color RED = new Color(255, 8, 0);
+	private static final Color BLUE = new Color(0, 127, 255);
+
+	private static final Color[] colorFor = { BROWN, null, GREEN, RED, BLUE };
+	
 	public static void main(String[] args) throws Throwable {
 		UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
 		JFrame frame = new JFrame("Treasure hunt - Triple your money!");
 		frame.setSize(800, 800);
 		frame.setLocation(200, 100);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setContentPane(new Gambling());
 		frame.setVisible(true);
 	}
